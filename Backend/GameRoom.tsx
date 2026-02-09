d3f4b500c8709604f6eb97f3ae4286af7b5502fe
ike-123 <onugha52@gmail.com>
2026-02-09 20:20:40 +0000

    Make multiplayer logic in Multiplayer.tsx work with the new typing engine. Fix start 'game starts in' timer display so that it finishes on the count of 0.

--- Backend/GameRoom.tsx
+++ Backend/GameRoom.tsx
@@ -7,7 +7,7 @@ type PlayerState = {
     progressIndex: number;
     wpm: number;
     finished: boolean;
-    finishtime:string;
+    finishtime: string;
 }
 
 type WordDoneData = {
@@ -20,15 +20,15 @@ export class GameRoom {
 
     io: Server;
     roomId: string;
-    players: Map<string | undefined,PlayerState>;
+    players: Map<string | undefined, PlayerState>;
     status: string;
     words: string[];
     startAt: number | null;
     countdownTimer: number;
 
 
-    
-    constructor(io:Server, roomId:string){
+
+    constructor(io: Server, roomId: string) {
 
         this.io = io;
         this.roomId = roomId;
@@ -42,86 +42,87 @@ export class GameRoom {
     }
 
 
-    addPlayer(socket: Socket):void {
+    addPlayer(socket: Socket): void {
 
         //add socket(client) to room
         socket.join(this.roomId);
 
         //Add newly joined player to the array of players
-        this.players.set(socket.id,{progressIndex:0,wpm:0,finished:false,finishtime:""})
+        this.players.set(socket.id, { progressIndex: 0, wpm: 0, finished: false, finishtime: "" })
 
-        this.io.to(this.roomId).emit("setWords",{"words":this.words});
+        this.io.to(this.roomId).emit("setWords", { "words": this.words });
 
         // send the updated array of players back to all the clients in room
-        this.io.to(this.roomId).emit("state",Array.from(this.players.entries()).map(([id,val]) =>({id,...val})));
+        this.io.to(this.roomId).emit("state", Array.from(this.players.entries()).map(([id, val]) => ({ id, ...val })));
 
         //send status of game to clients in room
-        this.io.to(this.roomId).emit("status",this.status);
+        this.io.to(this.roomId).emit("status", this.status);
 
 
-        if(this.players.size >= 2 && this.status === "waiting"){
+        if (this.players.size >= 2 && this.status === "waiting") {
 
             //generate random words
             // this.words = this.getRandomWords(10)
-            
+
             //set status to countdown and send to clients in room
             this.status = "countdown"
-            this.io.to(this.roomId).emit("status",this.status)
-    
+            this.io.to(this.roomId).emit("status", this.status)
+
 
             let countdown = 7;
-    
+
             const interval = setInterval(() => {
-                this.io.to(this.roomId).emit("countdown",countdown)
+                this.io.to(this.roomId).emit("countdown", countdown)
                 countdown--;
     
-                if(countdown <= -1 ){
+                if(countdown <= 0){
                     clearInterval(interval)
                     this.status = "running"
                     this.startAt = Date.now() + 500
-    
-                    this.io.to(this.roomId).emit("start",{"words":this.words,"startAt":this.startAt});
+
+                    this.io.to(this.roomId).emit("start", { "words": this.words, "startAt": this.startAt });
                     this.io.to(this.roomId).emit("state", Array.from(this.players.entries()).map(([id, val]) => ({ id, ...val })));
-                    this.io.to(this.roomId).emit("status",this.status)
-    
-    
+                    this.io.to(this.roomId).emit("status", this.status)
+
+
                 }
-    
+
             }, 1000);
         }
     }
 
-    HandleDisconnect(socket: Socket):void {
+    HandleDisconnect(socket: Socket): void {
 
         this.players.delete(socket.id);
 
-        if(this.status === "countdown"){
+        if (this.status === "countdown") {
 
-            if(this.players.size === 1){
+            if (this.players.size === 1) {
 
                 this.status = "waiting"
             }
         }
 
         this.io.to(this.roomId).emit("state", Array.from(this.players.entries()).map(([id, val]) => ({ id, ...val })));
-        this.io.to(this.roomId).emit("status",this.status)
-      
+        this.io.to(this.roomId).emit("status", this.status)
+
     }
 
-    HandleWordDone(socket:Socket, data:WordDoneData ){
-    
+    HandleWordDone(socket: Socket, data: WordDoneData) {
+
         const TargetPlayer = this.players.get(socket.id);
 
-        if(!TargetPlayer || this.status != "running")return;
+        if (!TargetPlayer || this.status != "running") return;
 
 
-        //if player has finished
-        if(data.nextIndex === TargetPlayer.progressIndex + 1){
+        if (data.nextIndex === TargetPlayer.progressIndex + 1) {
 
             TargetPlayer.progressIndex = data.nextIndex;
-            TargetPlayer.wpm =  Math.round((data.totalChars / 5) / (data.elapsedMs / 60000));
+            TargetPlayer.wpm = Math.round((data.totalChars / 5) / (data.elapsedMs / 60000));
 
-            if(TargetPlayer.progressIndex === this.words.length){
+
+            //if player has finished
+            if (TargetPlayer.progressIndex === this.words.length) {
 
                 TargetPlayer.finished = true;
                 TargetPlayer.finishtime = this.ConvertMillisecondsToMinutesAndSeconds(data.elapsedMs);
@@ -132,18 +133,18 @@ export class GameRoom {
 
     }
 
-    getRandomWords(amount:number){
-        const randomArray = [...wordsList].sort(() => 0.5-Math.random());
-        return randomArray.slice(0,amount)
+    getRandomWords(amount: number) {
+        const randomArray = [...wordsList].sort(() => 0.5 - Math.random());
+        return randomArray.slice(0, amount)
     }
 
-    ConvertMillisecondsToMinutesAndSeconds(milliseconds:number):string{
-    
+    ConvertMillisecondsToMinutesAndSeconds(milliseconds: number): string {
+
         const totalSeconds = Math.floor(milliseconds / 1000)
         const minutes = Math.floor(totalSeconds / 60);
         const seconds = totalSeconds % 60;
 
-        const paddedSeconds = String(seconds).padStart(2,'0');
+        const paddedSeconds = String(seconds).padStart(2, '0');
 
         const totaltime = `${minutes}:${paddedSeconds}`;
 
