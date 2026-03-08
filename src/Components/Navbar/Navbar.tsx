5aec15493cddc1ad49f2e6d4009360d42d7334bd
ike-123 <onugha52@gmail.com>
2026-03-08 02:04:25 +0000

    add Spinner component and update Navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,13 +1,56 @@
+import { useAuthStore } from '@/Stores/AuthStore'
 import React from 'react'
+import { Spinner } from '../ui/spinner'
 // import './Navbar.scss'
 
 const Navbar = () => {
+
+  const User = useAuthStore((state) => state.user)
+  const isLoading = useAuthStore((state) => state.loading)
+
+
+
+  // if (isLoading) {
+  //   return (
+  //     <Spinner />
+  //   )
+  // }
+
+  // if (!User) {
+  //   return (
+  //     <></>
+  //   )
+  // }
+
   return (
-    <div className='Navbar'>
-        <div className="container">
-            <div className="logo">Logo</div>
-            <div className="Links">Links</div>
-        </div>
+    <div className='Navbar  h-10'>
+      <div className="container flex justify-end">
+
+        {
+          isLoading ? (
+
+            <div className='w-10 h-10 flex justify-center items-center'>
+              <Spinner className='size-6'/>
+
+            </div>
+          )
+
+
+            :
+
+            (User
+
+              ?
+
+              <div className="logo">
+                <img className='rounded-full h-10 w-10 object-cover' src={User.image} alt="" />
+              </div>
+
+              : "")
+        }
+
+
+      </div>
     </div>
   )
 }
