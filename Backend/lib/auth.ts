3c84ffbc0d06fd933588865691711a9745334eb0
ike-123 <onugha52@gmail.com>
2026-03-25 23:24:59 +0000

    Install and setup Stripe NPM package and better-auth stripe Plugin.

--- Backend/lib/auth.ts
+++ Backend/lib/auth.ts
@@ -1,10 +1,18 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // import { PrismaClient } from "@prisma/client";
-import {prisma} from "./prisma"
+import { prisma } from "./prisma"
 
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
+
+import { stripe } from "@better-auth/stripe"
+import Stripe from "stripe"
+
+
+const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: "2026-02-25.clover", // Latest API version as of Stripe SDK v20.0.0
+})
 
 // const connectionString = `${process.env.DATABASE_URL}`;
 // const adapter = new PrismaPg({ connectionString });
@@ -23,11 +31,18 @@ export const auth = betterAuth({
     },
     trustedOrigins: ["http://localhost:5173"],
     baseURL: process.env.BETTER_AUTH_URL,
-    socialProviders: { 
-        google:{
+    socialProviders: {
+        google: {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
 
         }
-    }, 
+    },
+    plugins: [
+        stripe({
+            stripeClient,
+            stripeWebhookSecret: process.env.STRIPE_WEBHOOK_SECRET!,
+            createCustomerOnSignUp: true,
+        })
+    ]
 });
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,12 @@
 import { createAuthClient } from "better-auth/react"
+import { stripeClient } from "@better-auth/stripe/client"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3001"
+    baseURL: "http://localhost:3001",
+    plugins: [
+        stripeClient({
+            subscription: false //if you want to enable subscription management
+        })
+    ]
 })
