ede371321bde7360319b324909267028144285b0
ike-123 <onugha52@gmail.com>
2026-05-01 23:57:31 +0100

    Show Player stats and finish position upon test completion in Multiplayer mode.

--- src/Components/MultiplayerRaceTrack.tsx
+++ src/Components/MultiplayerRaceTrack.tsx
@@ -1,7 +1,8 @@
 import { useApplication, useTick } from '@pixi/react';
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import AnimatedSpriteAvatar from './AnimatedSpriteAvatar';
 import { type PlayerState } from "../Pages/Multiplayer"
+import { Assets, Texture } from 'pixi.js';
 
 type MultiplayerProps = {
     Players: PlayerState[],
@@ -10,6 +11,39 @@ type MultiplayerProps = {
 }
 const MultiplayerRaceTrack = (props: MultiplayerProps) => {
 
+    const [goldmedaltexture, setgoldmedalTexture] = useState<Texture>()
+
+
+    const VBox = ({ children, gap = 3 }: { children: React.ReactNode; gap?: number }) => (
+        <pixiContainer
+            zIndex={2}
+            ref={c => {
+                if (!c) return;
+                let y = 0;
+                c.children.forEach(child => {
+                    child.y = y;
+                    y += child.height + gap;
+                });
+            }}
+        >
+            {children}
+        </pixiContainer>
+    );
+
+    const HBox = ({ children, gap = 40 }: { children: React.ReactNode; gap?: number }) => (
+        <pixiContainer
+            ref={c => {
+                if (!c) return;
+                let x = 0;
+                c.children.forEach(child => {
+                    child.x = x;
+                    x += child.width + gap;
+                });
+            }}
+        >
+            {children}
+        </pixiContainer>
+    );
 
     const playerRef = useRef<any>(null);
 
@@ -26,6 +60,17 @@ const MultiplayerRaceTrack = (props: MultiplayerProps) => {
     })
 
     useEffect(() => {
+
+        async function loadAsset() {
+            const texture = await Assets.load('/gold-medal.png');
+
+            setgoldmedalTexture(texture);
+
+            //  const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
+
+        }
+
+        loadAsset();
 
         // const onResize = () => {
         //     console.log(app.app.screen.width)
@@ -57,6 +102,11 @@ const MultiplayerRaceTrack = (props: MultiplayerProps) => {
                     const progressPercent = (player.progressIndex / props.wordsLength);
                     const playerXPosition = props.wordsLength ? progressPercent * width * .95 : 0
 
+                    const trackColour = player.finished ? "0E3044" : "0E3044"
+
+                    //Limit presentable name to Maximum of 12 characters after add elipsis
+                    const playerName = player.DisplayName;
+
                     // console.log(progressPercent)
                     console.log(width);
                     // console.log(progressPercent)
@@ -64,7 +114,7 @@ const MultiplayerRaceTrack = (props: MultiplayerProps) => {
                     return (
                         <pixiGraphics key={player.id} y={index * (rectangleheight + 1)} draw={(graphics) => {
                             graphics.clear();
-                            graphics.setFillStyle({ color: '0E3044' });
+                            graphics.setFillStyle({ color: trackColour });
                             graphics.rect(0, 0, width * .95, rectangleheight);
                             graphics.fill();
                         }} >
@@ -92,19 +142,149 @@ const MultiplayerRaceTrack = (props: MultiplayerProps) => {
                             </pixiContainer>
 
 
+                            {
+
+                                player.finished &&
+
+                                <>
+
+                                    <pixiContainer ref={c => {
+                                        if (c) {
+                                            c.pivot.x = c.width / 2;
+                                        }
+                                    }}
+
+                                        x={(width * .95 / 2)}>
+
+                                        {/* {goldmedaltexture && <pixiSprite scale={50 / 750} y={25} x={-15} anchor={0.5} texture={goldmedaltexture} />} */}
+
+
+                                        <pixiText text={` ${playerName}`} style={{
+                                            fill: 'white',
+                                            fontSize: 18,
+                                        }}
+
+                                            y={15}
+                                            zIndex={2}
+                                        >
+
+                                        </pixiText>
+                                    </pixiContainer>
+
+
+                                    <pixiContainer x={(width * 0.95) * 0.75} y={5}>
+                                        {/* <pixiText
+                                    text="97 WPM"
+                                    anchor={.5}
+                                    style={{ fill: 'white', fontSize: 12 }}
+                                />
+
+                                <pixiText
+                                    text="100% Accuracy"
+                                    anchor={.5}
+                                    x={80} // offset from first label
+                                    style={{ fill: 'white', fontSize: 12 }}
+                                />
+
+                                <pixiText
+                                    text="time: 00:39"
+                                    anchor={.5}
+                                    x={180}
+                                    style={{ fill: 'white', fontSize: 12 }}
+                                /> */}
+                                        <HBox>
+
+
+                                            {/* <VBox>
+                                                <pixiText text="WPM" style={{ fill: 'gray', fontSize: 10 }} />
+                                                <pixiText text="97" style={{ fill: 'pink', fontSize: 18 }} />
+                                            </VBox> */}
+
+                                            {/* <VBox>
+                                                <pixiText text="Accuracy" style={{ fill: 'gray', fontSize: 9 }} />
+                                                <pixiText text="100%" style={{ fill: 'white', fontSize: 14 }} />
+                                            </VBox> */}
+
+                                            <VBox>
+                                                <pixiText text="Time" style={{ fill: 'gray', fontSize: 9 }} />
+                                                <pixiText text="00:39" style={{ fill: 'white', fontSize: 14 }} />
+                                            </VBox>
+
+                                            {goldmedaltexture && <pixiSprite zIndex={2} scale={50 / 750} y={22} x={width * .95 + ((width - width * .95) / 2)} anchor={0.5} texture={goldmedaltexture} />}
+                                        </HBox>
+
+                                    </pixiContainer>
+
+                                </>
+
+                            }
+
+
+
+
+
+
                             <pixiGraphics draw={(graphics) => {
                                 graphics.clear();
                                 graphics.setFillStyle({ color: '#081f30' });
-                                graphics.rect(width * .95, 0, width * .5, rectangleheight);
+                                graphics.rect(width * .9, 0, width * .5, rectangleheight);
                                 graphics.fill();
                                 graphics.zIndex = 1;
 
-                            }} >
 
+
+
+
+                            }} >
                             </pixiGraphics>
 
 
 
+                            {/* {
+                                player.finished
+
+                                    ?
+                                    goldmedaltexture && <pixiSprite zIndex={2} scale={50 / 750} y={25} x={width * .95 + ((width - width * .95) / 2)} anchor={0.5} texture={goldmedaltexture} />
+
+
+                                    :
+                                    <pixiContainer zIndex={2} x={width * .95 + ((width - width * .95) / 2)} y={7} >
+                                        <VBox>
+                                            <pixiText anchor={{ x: .5, y: 0 }} zIndex={3} text="WPM" style={{ fill: 'gray', fontSize: 9 }} />
+                                            <pixiText anchor={{ x: .5, y: 0 }} zIndex={3} text={player.wpm} style={{ fill: 'white', fontSize: 14 }} />
+                                        </VBox>
+                                    </pixiContainer>
+                            } */}
+
+                            <pixiContainer zIndex={2} x={width * .895 + ((width - width * .95) / 2)} y={7} >
+
+                                <HBox gap={18}>
+                                    <VBox>
+                                        <pixiText anchor={{ x: .5, y: 0 }} zIndex={3} text="WPM" style={{ fill: 'gray', fontSize: 9 }} />
+                                        <pixiText anchor={{ x: .5, y: 0 }} zIndex={3} text={player.wpm} style={{ fill: 'white', fontSize: 16 }} />
+                                    </VBox>
+
+                                    {
+
+                                        player.finished ??
+
+                                        <VBox>
+                                            <pixiText anchor={{ x: .5, y: 0 }} zIndex={3} text="Accuracy" style={{ fill: 'gray', fontSize: 9 }} />
+                                            <pixiText anchor={{ x: .5, y: 0 }} zIndex={3} text={"100%"} style={{ fill: 'white', fontSize: 12 }} />
+                                        </VBox>
+
+                                    }
+
+
+                                </HBox>
+
+
+                            </pixiContainer>
+
+
+
+
+                            {/* 
                             <pixiText text={player.wpm + " wpm"} style={{
                                 fill: 'white',
                                 fontSize: 12,
@@ -114,7 +294,12 @@ const MultiplayerRaceTrack = (props: MultiplayerProps) => {
                                 zIndex={2}
                             >
 
-                            </pixiText>
+                            </pixiText>  */}
+
+
+
+
+
 
                         </pixiGraphics>
 
