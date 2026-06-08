72943553c6cb1fcdab7f38f2ac1b4e0da536f257
ike-123 <onugha52@gmail.com>
2026-06-08 23:48:53 +0100

    Update SettingsStore.tsx

--- src/Stores/settingsStore.tsx
+++ src/Stores/settingsStore.tsx
@@ -1,22 +1,41 @@
+import { settings } from "node:cluster";
+import { create } from "zustand";
+
 export interface UserSettings {
-  account: {
-    username: string;
-    email: string;
-  };
+    account: {
+        username: string;
+        email: string;
+    };
 
-  personalization: {
-    theme: "light" | "dark";
-    accentColor: string;
-  };
+    personalization: {
+        theme: "light" | "dark";
+    };
 
-  keyboard: {
-    keyboardSounds: boolean;
-    showVirtualKeyboard: boolean;
-  };
-
-  audio: {
-    masterVolume: number;
-    musicVolume: number;
-    sfxVolume: number;
-  };
+    keyboard: {
+        keyboardSounds: boolean;
+    };
 }
+
+type UserStore = {
+    settings: UserSettings,
+}
+
+export const useSettingsStore = create<UserStore>((set) => ({
+
+    settings: {
+        account: {
+            username: "string",
+            email: "string"
+        },
+
+        personalization: {
+            theme: "dark"
+        },
+
+        keyboard: {
+            keyboardSounds: true
+
+        },
+
+    }
+}))
