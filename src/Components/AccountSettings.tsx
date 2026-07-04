4ee5cbb33ca3805610dad155fefbdf0db41fb688
ike-123 <onugha52@gmail.com>
2026-07-04 16:28:33 +0100

    Retrieve user settings for Account section in Settings page from AuthStore.
    Implement Nav menu functionality in settings page.

--- src/Components/AccountSettings.tsx
+++ src/Components/AccountSettings.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Input } from './ui/input'
+import { useAuthStore } from '@/Stores/AuthStore'
 
 const AccountSettings = () => {
+
+
+  const User = useAuthStore((state) => state.user);
+  const isLoading = useAuthStore((state) => state.loading)
+
   return (
-    <div>
-        <h1>Account Settings</h1>
+    <div w->
+      <h1 className='mb-10'>Account Settings</h1>
+
+
+      <div className='flex-col flex gap-3'>
+
+        <div className='flex gap-3 items-center'>
+          <label className='text-lg' htmlFor="">Username</label>
+          <Input value={User?.name} disabled />
+        </div>
+
+
+        <div className='flex gap-3 items-center'>
+          <label className='text-lg' htmlFor="">Email</label>
+          <Input value={User?.email} disabled />
+        </div>
+
+        <div className='flex gap-3 items-center'>
+          <label className='text-lg text-nowrap' htmlFor="">Key ammount</label>
+          <Input value={User?.Keys} disabled />
+        </div>
+
+
+      </div>
 
     </div>
   )
--- src/Pages/Settings.tsx
+++ src/Pages/Settings.tsx
@@ -1,20 +1,57 @@
 import AccountSettings from '@/Components/AccountSettings'
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import React, { useState } from 'react'
+
+type SettingsSection = "Account" | "Personalisation";
 
 const Settings = () => {
-  return (
-    <div className='max-w-7xl flex h-100 m-auto'>
-        <div className='bg-amber-200 flex-1'>
 
+
+    const [CurrentSection, SetCurrentSection] = useState<SettingsSection>("Account")
+
+    return (
+        <div className='max-w-7xl flex h-100 m-auto'>
+            <div className='bg-amber-200 flex-1'>
+
+
+                <div className='h-100 sticky top-0 h-screen'>
+
+                    <div className='flex flex-col gap-3'>
+
+
+                        <Button className={`text-2xl rounded-sm h-12 flex justify-start `} onClick={() => {SetCurrentSection("Account")}}>
+                            Account
+                        </Button>
+
+                        <Button className={`text-2xl rounded-sm h-12 flex justify-start `} onClick={() => {SetCurrentSection("Personalisation")}}>
+                            Personalisation
+                        </Button>
+
+                        {/* <Button className={`text-2xl rounded-sm h-12 flex justify-start `} onClick={() => { }}>
+                            Keys
+                        </Button> */}
+
+
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div className='bg-neutral-900 flex-5 text-3xl font-bold'>
+
+                {
+                    CurrentSection === "Account" && <AccountSettings />
+                }
+
+                {
+                    CurrentSection === "Personalisation" && <>Personalisation</>
+                }
+
+
+            </div>
         </div>
-
-        <div className='bg-neutral-900 flex-5 text-3xl font-bold'>
-
-
-            <AccountSettings/>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Settings
